c67aaf61d9f33c1dac86ab901d600317edc57ca3
Tenacious-Qi <joe.mccann@mailbox.org>
2020-01-30 16:05:10 -0500

    added lyricsDisplay and comments

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+// -- AUDIO SELECTORS -- //
 const songTime = document.getElementsByTagName('audio')[0].getAttribute('id');
 const time = document.getElementById(songTime);
-
 const songGig = document.getElementsByTagName('audio')[1].getAttribute('id');
 const gig = document.getElementById(songGig);
-
 const songSolo = document.getElementsByTagName('audio')[2].getAttribute('id');
 const solo = document.getElementById(songSolo);
+// -- END AUDIO SELECTORS -- //
 
 const lyrics = document.querySelector('.lyrics');
-
 
 
 function setDate() {
@@ -24,7 +23,7 @@ function setDate() {
 
   if (seconds === 0) {
     secondHand.style.transition = "none";
-  } // these statements only seem to work if minutes !== 0. Clock only does weird reset thing when time.play() is called.
+  } // these statements only seem to work if minutes !== 0. 
   else if (seconds > 0) {
     secondHand.style.transition = "all .2s";
   }
@@ -32,25 +31,35 @@ function setDate() {
   const minutes = now.getMinutes();
   const minsDegrees = ((minutes / 60) * 360) + 90;
   minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  if (minutes === 0) {
+    secondHand.style.transition = "none";
+  } else if ((minutes === 0) && (seconds > 0)) {
+    secondHand.style.transition = "all .2s";
+  }
+
   
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   
   function playMusic () {
+    
+    const lyricsDisplay = lyrics.style.display = "block";
+
     if ((minutes === 0)  && (seconds < 34)) {
       time.play();
-      lyrics.style.display = 'block';
+      lyricsDisplay;
       lyrics.textContent = 'ticking away the moments that make up a dull day . . . ';
       
     } else if ((minutes === 30) && (seconds < 31)) {
       solo.play();
-      lyrics.style.display = 'block';
+      lyricsDisplay;
       lyrics.textContent = 'And you run and you run to catch up with the sun, but it\'s sinking . . . ';
   
     } else if (((minutes === 15) || (minutes == 45)) && (seconds < 15)) {
       gig.play();
-      lyrics.style.display = 'block';
+      lyricsDisplay;
       lyrics.textContent = 'Why should I be frightened of dying? There\'s no reason for it, you\'ve gotta go sometime . . . ';
   
     } else {
@@ -61,6 +70,3 @@ function setDate() {
 }
 
 setInterval(setDate, 1000);
-
-
-
